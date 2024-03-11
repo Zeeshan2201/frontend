709ccf3eba3af4ba34700dc7c8c7586f49dcc6c9
Faisal-Imran01 <faisalimransyed01@gmail.com>
2024-03-11 15:20:34 +0530

    faisalfile

--- src/Components/Hero/Faisal.jsx
+++ src/Components/Hero/Faisal.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Faisal = () => {
   return (
     <div>
-      <div className="hero-right">
+      <div className="card">
+
+        <div className="heading">
+            <h1>Happy Club</h1>
+        </div>
+
+        <div className="description">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae autem aspernatur, est, recusandae beatae, sequi cum asperiores fugit consequuntur </p>
+        </div>
         
       </div>
     </div>
