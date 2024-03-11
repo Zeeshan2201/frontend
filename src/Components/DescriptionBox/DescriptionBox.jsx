c053672fcf1f58cd3b89ca38346f946b502ce8c3
Zeeshan2201 <sheikhzeeshan608@gmail.com>
2024-03-11 11:29:16 +0530

    all

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio aspernatur vero asperiores mollitia saepe aliquid possimus, amet sunt accusantium numquam vitae maxime necessitatibus esse doloremque dignissimos unde ducimus pariatur officia officiis in, quibusdam ipsum reprehenderit et aliquam? Adipisci accusantium at dolore recusandae ratione odio dolores debitis pariatur minima numquam quibusdam accusamus sequi tempora inventore, quisquam eveniet magnam deserunt, fugit ducimus labore aperiam quos totam autem aut! Reiciendis illum, et error mollitia iusto quo dolorum adipisci necessitatibus veritatis distinctio laboriosam ab perferendis amet inventore debitis animi doloremque ipsam praesentium ad molestiae labore repellendus? Totam quidem nulla expedita in porro vitae saepe!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae veniam libero, placeat deserunt consequuntur ut perferendis dolor commodi soluta harum? Blanditiis numquam quos suscipit, distinctio corrupti tempore, reprehenderit aperiam quidem vel, quam officiis nulla commodi eaque facere veniam. Quos fugit vero quo animi explicabo tempora eius sequi non reiciendis, quaerat molestiae, temporibus laborum molestias quasi iure adipisci laboriosam amet optio atque iusto, eaque iste? Nihil rem, quisquam et doloribus numquam voluptatum quaerat maiores nisi. Itaque autem atque ipsam neque porro facilis deleniti aliquam consectetur. Numquam magnam cupiditate laborum amet nam nihil blanditiis perspiciatis. Soluta sapiente nam, natus nulla quo quam!</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
