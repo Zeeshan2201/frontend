d9ccc0c8656fce20f51dbedcf6ec512b570f2475
Zeeshan2201 <sheikhzeeshan608@gmail.com>
2024-03-15 11:35:39 +0530

    remove

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,8 +10,6 @@ import Faisal from '../Components/Hero/Faisal'
 const Shop = () => {
   return (
     <div>
-      {/* hello */}
-      {/* hey zeeshan */}
       <Hero/>
       <Popular/>
       <Offers/>
